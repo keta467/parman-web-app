0c4a9a52b1ad6f24254cf15381a8a3f3fc150596
keta467 <irie@lambda.co.jp>
2022-07-12 16:52:30 +0900

    save: 20220712

--- src/pages/view_file_all/ViewFileAll.jsx
+++ src/pages/view_file_all/ViewFileAll.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import FileAllTable from '../../components/file_all_table/FileAllTable';
 import Topbar from '../../components/topbar/Topbar'
+import { moduleList ,installVersionInfo} from '../../dummyData';
+import "./ViewFileAll.css";
 
 export default function ViewFileAll({ titletext }) {
+
+  //モジュールリストの作成
+  var modulelist = [];
+  for(var i = 0; i < moduleList.data.length; i++){
+    modulelist.push(moduleList.data[i][1]);
+  }
+
+  //PCリストの作成
+  var pclist = [];
+  for(var i = 0; i < installVersionInfo.data.length; i++){
+    var module = installVersionInfo.data[i];
+
+    var pcname = module[1];
+    var pcversions = [];
+    for(var j = 2; j < module.length; j++){
+      pcversions.push(module[j]);
+    }
+    pclist.push({name:pcname, versions:pcversions});
+  }
+
+  console.log("確認");
+  // for(var i = 0; i < modulelist.length; i++){
+  //   console.log(modulelist[i]);
+  // }
+  // for(var i = 0; i < pclist.length; i++){
+  //   console.log(pclist[i].versions[0]);
+  // }
+
+
   return (
     <>
       <Topbar titletext={titletext} />
+      <div className='buttonwrapper'>
+        <button>更新</button>
+      </div>
+      <FileAllTable modulelist={modulelist} pclist={pclist} />
     </>
   )
 }
