e47bc4128b62ee9f6db0f9b847e6eee30d82a2db
keta467 <irie@lambda.co.jp>
2022-07-22 17:41:34 +0900

    modify: dev edit

--- src/pages/manage_package/ManagePackage.jsx
+++ src/pages/manage_package/ManagePackage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react';//dev
 import Topbar from '../../components/topbar/Topbar';
 import TreeView from '../../components/treeview/TreeView';
 import "./ManagePackage.css";
