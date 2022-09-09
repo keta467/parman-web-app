73f0bd950230a9f2eb0f72a879c95f4c9188fac0
keta467 <irie@lambda.co.jp>
2022-09-09 12:24:49 +0900

    build: 2022年9月9日1224

---
+++ const.js
@@ -0,0 +1,10 @@
+//デバッグモード true => ダミーデータ参照   false => API参照
+export const DebugMode = false;
+
+// 環境１　ip: localhost　port: 3002
+// 環境２　ip: 3.137.85.73　port: 3002
+const API_SERVER_IP = "3.137.85.73";
+const PORT = 3002;
+
+// axios baseURL
+export const BaseURL = `http://${API_SERVER_IP}:${PORT}`;
